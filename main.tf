3cc32120bd87dba491a1179d26c6beab354b1827
kirillosgeorge <kirillosgeorge70@gmail.com>
2022-10-30 00:11:54 +0200

    improvement

--- main.tf
+++ main.tf
@@ -1,142 +1,131 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.29.1"
-    }
-    tls = {
-      source = "hashicorp/tls"
-      version = "~>4.0"
-    }
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
 
-provider "azurerm" {
-    version = "3.29.1"
-    alias = "networking"
-    subscription_id = var.subscription_id
-    client_id = var.client_id 
-    client_secret = var.client_secret
-  
+# Create network interface
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+  }
 }
 
-resource "azurerm_resource_group" "luftborntest" {
-    name = "luftbormtest"
-    location = "eastus"
-  
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
 }
 
-resource "azurerm_virtual_network" "luftborntest" {
-    name = "luftborntest-network"
-    address_space = ["10.0.0.0/16"]
-    location = azurerm_resource_group.luftborntest.location
-    resource_group_name = azurerm_resource_group.luftborntest.name
-  
+# Generate random text for a unique storage account name
+resource "random_id" "random_id" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 8
 }
 
-resource "azurerm_subnet" "luftborntest" {
-    name = "internal"
-    resource_group_name = azurerm_resource_group.luftborntest.name
-    virtual_network_name = azurerm_virtual_network.luftborntest.name
-    address_prefixes = ["10.0.2.0/24"]  
-}
-resource "azurem_public_ip" "public_ip" {
-    name = "vm_public_ip"
-    azurerm_resource_group = azurerm_resource_group.luftborntest.name
-    location = azurerm_resource_group.luftborntest.location
-    allocation_method = "Dynamic"
-  
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "my_storage_account" {
+  name                     = "diag${random_id.random_id.hex}"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-resource "azurerm_network_interface" "luftborntest" {
-    name = "luftborntest-nic"
-    location = azurerm_resource_group.luftborntest.location
-    resource_group_name = azurerm_resource_group.luftborntest.name
-
-    ip_configuration {
-      name = "internal"
-      subnet_id = azurerm_subnet.luftborntest.id
-      private_ip_address_allocation = "Dynamic"
-      public_ip_address_id = azurerm_public_ip.public_ip.id
-    }
-
-  
+# Create (and display) an SSH key
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-resource "azurerm_network_security_group" "luftborntest" {
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
+  name                  = "myVM"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  size                  = "Standard_DS1_v2"
 
-    name = "luftborntestNSG1"
-    location = azurerm_resource_group.luftborntest.location
-    resource_group_name = azurerm_resource_group.luftborntest.name
-  
-}
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
 
-resource "azurem_network_security_rule" "luftborntest" {
-    name = "luftborntestNSR1"
-    priority = 100
-    direction = "Inboud"
-    access = "Allow"
-    protocal = "Tcp"
-    sourco_port_range = "*"
-    destination_port_range = "22"
-    sourco_address_prefix = "*"
-    destination_address_prefix = "*"
-    resource_group_name = azurerm_resource_group.luftborntest.name 
-    azurerm_network_security_group = azurerm_network_security_group.luftborntest.name
-  
-}
-
-resource "azurerm_network_interface_security_group_association" "luftborntest" {
-    network_interface_id = azurerm_network_interface.luftborntest.id
-    network_security_group_id = azurerm_network_security_group.luftborntest.id
-  
-}
-
-resource "tls_private_key" "sshkey" {
-    algorithm = "RSA"
-    rsa_bits = 4096
-  
-}
-
-resource "azurerm_linux_virtual_machine" "luftborntest" {
-    name = "luftborntest-vm"
-    resource_group_name = azurerm_resource_group.luftborntest.name
-    location = azurerm_resource_group.luftborntest.location
-    size = "Standard_B1s"
-    admin_username = "root"
-    disable_password_authentication = true
-
-    network_interface_ids = [azurerm_network_interface.luftborntest.id]
-
-    source_image_reference {
+  source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "18.04-LTS"
     version   = "latest"
   }
 
-  os_disk {
-      caching = "ReadWrite"
-      storage_account_type = "standard_LRS"
-    }
+  computer_name                   = "myvm"
+  admin_username                  = "azureuser"
+  disable_password_authentication = true
 
-    admin_ssh_key {
-      username = "root"
-      public_key = tls_private_key.sshkey.public_key_openssh
-    }
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.example_ssh.public_key_openssh
+  }
 
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
+  }
 }
-
-output "resource_group_name" {
-  value = azurerm_resource_group.luftborntest.name
-}
-
-output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.luftborntest.public_ip_address
-}
-
-output "tls_private_key" {
-  value     = tls_private_key.example_ssh.private_key_pem
-  sensitive = true
-}
-
